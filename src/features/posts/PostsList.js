8d93adde691ec4e43e72984246e164bc95fe549b
edubew <edubew98@gmail.com>
2022-10-06 16:13:11 +0300

    Make the website responsive

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -13,7 +13,7 @@ const PostsList = () => {
   const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
+      <p className="post__card">{post.content.substring(0, 100)}</p>
       <p className="post__credits">
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
